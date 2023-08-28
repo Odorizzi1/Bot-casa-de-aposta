44583f991d60d8ce4de324833f401d202000aa7c
Odorizzi1 <88588959+Odorizzi1@users.noreply.github.com>
2023-08-27 23:25:22 -0300

    Update mensagem.py

--- mensagem.py
+++ mensagem.py
@@ -1,9 +1,16 @@
 from datetime import datetime, timedelta
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup
-# Calcule o horário de expiração (3 minutos a partir da hora atual)
-agora = datetime.now()
-horario_expiracao = agora + timedelta(minutes=3)
-horario_expiracao_str = horario_expiracao.strftime('%H:%M')
+import pytz
+
+# Define o fuso horário do Brasil (BRT - Horário de Brasília)
+fuso_horario_brasil = pytz.timezone('America/Sao_Paulo')
+
+# Obtém a hora atual no fuso horário do Brasil
+agora_brasil = datetime.now(fuso_horario_brasil)
+
+# Calcula o horário de expiração (3 minutos a partir da hora atual)
+horario_expiracao_brasil = agora_brasil + timedelta(minutes=3)
+horario_expiracao_str = horario_expiracao_brasil.strftime('%H:%M')
 
 url_mines = "https://greenbets.io/casino/game/1695257/?bta=45274&brand=greenbetsio"
 url_cadastro = "https://afiliados.greenbets.io/visit/?bta=45274&brand=greenbetsio"
